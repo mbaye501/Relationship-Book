3352c44422b16d446db1d3aac65f12562f815aba
mbaye501 <smartmbaye@gmail.com>
2020-06-17 19:30:48 -0400

    added some header style

--- App.js
+++ App.js
@@ -2,44 +2,50 @@
 
 //#region import react standard libs
 import React from "react";
+import { AppLoading } from 'expo';
+import { useFonts, Merienda_400Regular, Merienda_700Bold } from '@expo-google-fonts/merienda';
+import { Courgette_400Regular } from '@expo-google-fonts/courgette';
+import { KaushanScript_400Regular } from '@expo-google-fonts/kaushan-script';
+import { Tangerine_400Regular, Tangerine_700Bold } from '@expo-google-fonts/tangerine';
 //#endregion
 
 //#region import my modules
-import { colors } from './src/Styles'
-import { Incon } from './src/Components'
-import { ContactsStack, HomeStack, Menu, MessagesStack } from './src/Routes'
+import { MainAppTabs, AuthStack } from './src/Routes'
+
 //#endregion
 
 //#region import Navigations libs
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 //#endregion
 
 //#endregion
 
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+let isSignedIn = true
+
 
 function App() {
+  //#region load fonts
+  let [fontsLoaded] = useFonts({
+    Merienda_400Regular,
+    Merienda_700Bold,
+    Courgette_400Regular,
+    KaushanScript_400Regular,
+    Tangerine_400Regular,
+    Tangerine_700Bold
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  //#endregion
+
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            return <Incon iconName={route.name} size={size} color={color} focused={focused} />
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: colors.DarkBlue,
-          inactiveTintColor: colors.DarkGray,
-        }}
-      >
-
-        <Tab.Screen name="Home" component={HomeStack} />
-        <Tab.Screen name="Messages" component={MessagesStack} />
-        <Tab.Screen name="Contacts" component={ContactsStack} />
-        <Tab.Screen name="Menu" component={Menu} />
-      </Tab.Navigator>
+      {isSignedIn ? <MainAppTabs /> : <AuthStack />}
     </NavigationContainer>);
 }
 
